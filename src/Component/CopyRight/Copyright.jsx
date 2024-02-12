59e8413a96ab5f23aa2f06ca9602e533010b7163
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-02-12 10:42:33 +0530

    Copy right part update

--- src/Component/CopyRight/Copyright.jsx
+++ src/Component/CopyRight/Copyright.jsx
@@ -4,14 +4,8 @@ function Copyrightfooter() {
   return (
     <div className="col-12">
       <div className="col-12 d-flex p-4 bg-secondary ">
-        <div className="col-6 text-white">
+        <div className="col-12 text-white d-flex flex-column">
           © 2024 Recycled Shoe Store. Powered by Recycled Shoe Store.
-        </div>
-        <div className="col-6" style={{ cursor: "pointer" }}>
-          <img
-            src="https://websitedemos.net/recycled-shoe-store/wp-content/uploads/sites/983/2021/11/payment-icons.png"
-            alt=""
-          />
         </div>
       </div>
     </div>
